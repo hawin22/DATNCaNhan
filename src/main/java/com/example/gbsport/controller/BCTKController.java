65ed6e483203b21fd84f97410e691b16bd0dadc1
hawin22 <146153289+hawin22@users.noreply.github.com>
2025-02-18 20:42:02 +0700

    hh

--- src/main/java/com/example/gbsport/controller/BCTKController.java
+++ src/main/java/com/example/gbsport/controller/BCTKController.java
@@ -1,2 +1,7 @@
-package com.example.gbsport.controller;public class BCTKController {
+package com.example.gbsport.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BCTKController {
 }
--- src/main/java/com/example/gbsport/entity/HoaDonChiTiet.java
+++ src/main/java/com/example/gbsport/entity/HoaDonChiTiet.java
@@ -1,2 +1,19 @@
-package com.example.gbsport.entity;public class HoaDonChiTiet {
+package com.example.gbsport.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "hoa_don_chi_tiet")
+public class HoaDonChiTiet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_hoa_don_chi_tiet;
+    private Integer id_chi_tiet_san_pham;
+    private Integer so_luong;
+    private double don_gia;
+    @ManyToOne
+    @JoinColumn(name = "id_hoa_don")
+    private HoaDon hoaDon;
 }

2202ccd8f50e298440918889d5a3a339a87abecd
nghianhph46340 <146502380+nghianhph46340@users.noreply.github.com>
2025-03-04 12:01:02 +0700

    ha

--- src/main/java/com/example/gbsport/controller/MailController.java
+++ src/main/java/com/example/gbsport/controller/MailController.java
@@ -1,2 +1,23 @@
-package com.example.gbsport.controller;public class MailController {
+package com.example.gbsport.controller;
+
+import com.example.gbsport.service.MailService;
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MailController {
+    @Autowired
+    MailService mailService;
+
+    @PostMapping("/send")
+    public String sendEmail(@RequestParam(name = "to") String to,
+                            @RequestParam(name = "subject") String subject,
+                            @RequestParam(name = "body") String body) {
+        mailService.sendEmail(to, subject, body);
+        System.out.println("đến:"+to +"\n"+"Tiêu đề:"+subject+"\n"+"Nội dung:"+body);
+        return "Email đã gửi thành công!";
+    }
 }
